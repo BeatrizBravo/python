55b7a3062d512a5a1a5e3b7efbbda067a58a99b3
Beatriz Bravo <beatriz.bravo-chavez@ten10.com>
2023-06-15 21:58:07 +0100

    added a for loop in a range and a function

---
+++ CFG ejercicios/Session2.py
@@ -0,0 +1,16 @@
+for number in range(100):
+    output = 'o' * number
+    print(output)
+
+
+##############
+
+
+def calculate_vat(amount):
+ return amount *1.2
+
+total = calculate_vat(100)
+
+print(total)
+
+
