d0bdb654e5654bf6c525d178cd5d54b2efec8b19
Beatriz Bravo <beatriz.bravo-chavez@ten10.com>
2023-06-22 21:23:46 +0100

    added while loop to ask if the user  wants to carry on playing

--- Projects/rock-paper-scissor.py
+++ Projects/rock-paper-scissor.py
@@ -9,43 +9,49 @@ def rps():
     else:
         choice = "scissors"
     return choice
+play_again = "y"
+# asking if the user  wants to carry on playing
+while play_again.lower() == "y":
+    print('''
+        Welcome to Rock Paper Scissors Game!
+        Choose your move:
+        1 - Rock
+        2 - Paper
+        3 - Scissors
+    ''')
 
-print('''
-    Welcome to Rock Paper Scissors Game!
-    Choose your move:
-    1 - Rock
-    2 - Paper
-    3 - Scissors
-''')
+    user_choice = int(input("Enter your move: "))
 
-user_choice = int(input("Enter your move: "))
+    #user's choice is not one of the three valid options in the game
+    if user_choice not in [1, 2, 3]:
+        print("Invalid move. Please try again.")
+    else:
+        computer_choice = rps()
+        if user_choice == 1:
+            print("You chose Rock.")
+            if computer_choice == "rock":
+                print("Computer also chose Rock. It's a tie!")
+            elif computer_choice == "paper":
+                print("Computer chose Paper. You lose!")
+            elif computer_choice == "scissors":
+                print("Computer chose Scissors. You win!")
+        elif user_choice == 2:
+            print("You chose Paper.")
+            if computer_choice == "rock":
+                print("Computer chose Rock. You win!")
+            elif computer_choice == "paper":
+                print("Computer also chose Paper. It's a tie!")
+            elif computer_choice == "scissors":
+                print("Computer chose Scissors. You lose!")
+        elif user_choice == 3:
+            print("You chose Scissors.")
+            if computer_choice == "rock":
+                print("Computer chose Rock. You lose!")
+            elif computer_choice == "paper":
+                print("Computer chose Paper. You win!")
+            elif computer_choice == "scissors":
+                print("Computer also chose Scissors. It's a tie!")
 
-#user's choice is not one of the three valid options in the game
-if user_choice not in [1, 2, 3]:
-    print("Invalid move. Please try again.")
-else:
-    computer_choice = rps()
-    if user_choice == 1:
-        print("You chose Rock.")
-        if computer_choice == "rock":
-            print("Computer also chose Rock. It's a tie!")
-        elif computer_choice == "paper":
-            print("Computer chose Paper. You lose!")
-        elif computer_choice == "scissors":
-            print("Computer chose Scissors. You win!")
-    elif user_choice == 2:
-        print("You chose Paper.")
-        if computer_choice == "rock":
-            print("Computer chose Rock. You win!")
-        elif computer_choice == "paper":
-            print("Computer also chose Paper. It's a tie!")
-        elif computer_choice == "scissors":
-            print("Computer chose Scissors. You lose!")
-    elif user_choice == 3:
-        print("You chose Scissors.")
-        if computer_choice == "rock":
-            print("Computer chose Rock. You lose!")
-        elif computer_choice == "paper":
-            print("Computer chose Paper. You win!")
-        elif computer_choice == "scissors":
-            print("Computer also chose Scissors. It's a tie!")
+        play_again = input("Do you want to play again? (Y/N): ")
+#end of the game
+print("Thanks for playing!")
