d7a8513f5dde7e28911bb6ad3b0b1ba13698c3e6
Beatriz Bravo <beatriz.bravo-chavez@ten10.com>
2023-06-22 21:20:29 +0100

    added a condition to find out the winner

--- Projects/rock-paper-scissor.py
+++ Projects/rock-paper-scissor.py
@@ -20,3 +20,32 @@ print('''
 
 user_choice = int(input("Enter your move: "))
 
+#user's choice is not one of the three valid options in the game
+if user_choice not in [1, 2, 3]:
+    print("Invalid move. Please try again.")
+else:
+    computer_choice = rps()
+    if user_choice == 1:
+        print("You chose Rock.")
+        if computer_choice == "rock":
+            print("Computer also chose Rock. It's a tie!")
+        elif computer_choice == "paper":
+            print("Computer chose Paper. You lose!")
+        elif computer_choice == "scissors":
+            print("Computer chose Scissors. You win!")
+    elif user_choice == 2:
+        print("You chose Paper.")
+        if computer_choice == "rock":
+            print("Computer chose Rock. You win!")
+        elif computer_choice == "paper":
+            print("Computer also chose Paper. It's a tie!")
+        elif computer_choice == "scissors":
+            print("Computer chose Scissors. You lose!")
+    elif user_choice == 3:
+        print("You chose Scissors.")
+        if computer_choice == "rock":
+            print("Computer chose Rock. You lose!")
+        elif computer_choice == "paper":
+            print("Computer chose Paper. You win!")
+        elif computer_choice == "scissors":
+            print("Computer also chose Scissors. It's a tie!")
