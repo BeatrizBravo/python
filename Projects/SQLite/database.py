a6accbb1efafb8a32ecec7e2811695340510c511
Beatriz Bravo <beatriz.bravo-chavez@ten10.com>
2023-09-07 18:53:15 +0100

    Added a for loop to view in the console the table by row

--- Projects/SQLite/database.py
+++ Projects/SQLite/database.py
@@ -1,7 +1,7 @@
 import sqlite3 #it doesn't need to be installed
 
 # create a connection with database and we create the database
-connection_with_db = sqlite3.connect('students1.db')
+connection_with_db = sqlite3.connect('students.db')
 
 cursor_db = connection_with_db.cursor()  # allow us to iterate  data row-by-row
 
@@ -24,8 +24,10 @@ cursor_db.executemany("INSERT INTO students VALUES (?, ?, ?)", all_students) #ad
 
 # QUERIES: select data
 cursor_db.execute("SELECT * FROM students")
-print(cursor_db.fetchall()) # To view the result on the console
-
+# print(cursor_db.fetchall()) # To view the result on the console
+table = cursor_db.fetchall()
+for row in table:
+    print(row)
 # commit: save all the change we have done
 connection_with_db.commit()
 
